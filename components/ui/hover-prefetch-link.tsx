0ca384a5a3f1a7d5c97dc055909ab13cd150b915
Aurora Scharff <aurora.sofie@gmail.com>
2026-08-14 14:59:13 +0200

    update

---
+++ components/ui/hover-prefetch-link.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import Link from 'next/link';
+import { useState } from 'react';
+import type { Route } from 'next';
+
+type Props<T extends string = string> = Omit<React.ComponentProps<typeof Link>, 'href' | 'prefetch'> & {
+  href: Route<T> | URL;
+};
+
+export function HoverPrefetchLink<T extends string>({ href, onFocus, onMouseEnter, onTouchStart, ...props }: Props<T>) {
+  const [intent, setIntent] = useState(false);
+
+  return (
+    <Link
+      {...props}
+      href={href as Route}
+      onFocus={event => {
+        setIntent(true);
+        onFocus?.(event);
+      }}
+      onMouseEnter={event => {
+        setIntent(true);
+        onMouseEnter?.(event);
+      }}
+      onTouchStart={event => {
+        setIntent(true);
+        onTouchStart?.(event);
+      }}
+      prefetch={intent ? true : null}
+    />
+  );
+}
